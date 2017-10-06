34dab1ab8b25c45cd3277667fcd38a0f69a5953c
Eddie Bickham <bickhame@gmail.com>
2017-10-06 05:44:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays() {
   return "Happy holidays!";
 };
 function happyHolidaysTo(name) {
-  var name = you;
+  var name = "you";
   return `Happy holidays, ${name}!`;
 };
 function happyHolidayTo(name, holiday) {
@@ -14,5 +14,5 @@ function happyHolidayTo(name, holiday) {
 function holidayCountdown(holiday, days) {
   var days = 20;
   var holiday = "Mother\'s Day";
-  return `It's ${days} days to ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`;
 };
